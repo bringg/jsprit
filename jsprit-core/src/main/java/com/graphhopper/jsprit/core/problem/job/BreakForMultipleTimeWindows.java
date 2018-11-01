d7d15178a835cf1e55a0b6bee4e7cfd36a0fadab
Kandel Irina <irina@bringg.com>
2018-11-01 15:35:14 +0200

    BreakForMultipleTimeWindows

---
+++ jsprit-core/src/main/java/com/graphhopper/jsprit/core/problem/job/BreakForMultipleTimeWindows.java
@@ -0,0 +1,7 @@
+package com.graphhopper.jsprit.core.problem.job;
+
+public class BreakForMultipleTimeWindows extends Break {
+    BreakForMultipleTimeWindows(Builder builder) {
+        super(builder);
+    }
+}
