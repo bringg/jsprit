a02696dde4f6fa1b51960e88fc7e4e34e9a4b139
Kandel Irina <irina@bringg.com>
2018-11-01 19:12:38 +0200

    extend from service

--- jsprit-core/src/main/java/com/graphhopper/jsprit/core/problem/job/BreakForMultipleTimeWindows.java
+++ jsprit-core/src/main/java/com/graphhopper/jsprit/core/problem/job/BreakForMultipleTimeWindows.java
@@ -1,20 +1,58 @@
 package com.graphhopper.jsprit.core.problem.job;
 
-public class BreakForMultipleTimeWindows extends Break {
+import com.graphhopper.jsprit.core.problem.Capacity;
+import com.graphhopper.jsprit.core.problem.Skills;
 
-    BreakForMultipleTimeWindows(Break.Builder builder) {
-        super(builder);
-    }
+public class BreakForMultipleTimeWindows extends Service {
 
-    public static class Builder extends Break.Builder {
+    public static class Builder extends Service.Builder<BreakForMultipleTimeWindows> {
+
+        /**
+         * Returns a new instance of builder that builds a pickup.
+         *
+         * @param id the id of the pickup
+         * @return the builder
+         */
+        public static BreakForMultipleTimeWindows.Builder newInstance(String id) {
+            return new BreakForMultipleTimeWindows.Builder(id);
+        }
+
+        private boolean variableLocation = true;
+
         Builder(String id) {
             super(id);
         }
 
+        /**
+         * Builds Pickup.
+         * <p>
+         * <p>Pickup type is "pickup"
+         *
+         * @return pickup
+         * @throws IllegalStateException if neither locationId nor coordinate has been set
+         */
         public BreakForMultipleTimeWindows build() {
-            super.build();
+            if (location != null) {
+                variableLocation = false;
+            }
+            this.setType("break");
+            super.capacity = Capacity.Builder.newInstance().build();
+            super.skills = Skills.Builder.newInstance().build();
             return new BreakForMultipleTimeWindows(this);
         }
+
     }
+
+    private boolean variableLocation = true;
+
+    BreakForMultipleTimeWindows(BreakForMultipleTimeWindows.Builder builder) {
+        super(builder);
+        this.variableLocation = builder.variableLocation;
+    }
+
+    public boolean hasVariableLocation() {
+        return variableLocation;
+    }
+
 
 }
